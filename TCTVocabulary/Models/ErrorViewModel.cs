9ad949c4ae1c819921fedd35c6e93bae16b327f5
Châu Nguyễn Bảo <2424802010046@student.tdmu.edu.vn>
2026-01-19 19:55:29 +0700

    Add project files.

---
+++ TCTVocabulary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TCTVocabulary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
