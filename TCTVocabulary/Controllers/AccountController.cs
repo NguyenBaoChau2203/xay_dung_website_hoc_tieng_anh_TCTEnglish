a3fb7d2d24cbc9adc2912af4be73ee43810b9e9f
Châu Nguyễn Bảo <2424802010046@student.tdmu.edu.vn>
2026-01-22 20:57:36 +0700

    - Add AccountController.cs with basic GET methods for Login and Register. - Create Views/Account directory. - Add Login.cshtml and Register.cshtml templates. - Add Loader.cshtml for loading screen animation. - Initialize image asset directory for project logo.

---
+++ TCTVocabulary/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TCTVocabulary.Models;
+
+namespace TCTVocabulary.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // GET: /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
