d5ef281e743478584b81695dba6b27fce29c9e90
Châu Nguyễn Bảo <2424802010046@student.tdmu.edu.vn>
2026-02-01 16:43:35 +0700

    Seed default user and update connection string
    Added EF Core migrations to seed a default user in the Users table with static CreatedAt date. Updated DbContext configuration and model snapshot to include seed data. Changed connection string in appsettings.json to use the correct server name.

---
+++ TCTVocabulary/Migrations/20260201093725_FixedSeedDataWithStaticDate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TCTVocabulary.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedSeedDataWithStaticDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserID",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 2, 1, 16, 20, 51, 117, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserID",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 2, 1, 16, 33, 28, 913, DateTimeKind.Local).AddTicks(3678));
+        }
+    }
+}
